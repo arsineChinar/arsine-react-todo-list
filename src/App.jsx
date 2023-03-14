a276112f09439f089dd76695bfbafcca74435f59
arsineChinar <arsine.chinaryan3@gmail.com>
2023-03-14 18:04:30 +0400

    homework62

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,50 @@
+import {Component} from 'react';
 import './App.css';
 import Product from "./products/Product";
 
-function App() {
-  return (
-    <div className="App">
-      <Product 
-      name="Banana"
-      price="2$"
-      description="Fresh bananas from Ecuador"
-      />
+class App extends Component{
 
-      <Product 
-      name="Apple"
-      price="1$"
-      description="Fresh apples"
-      />
+  state = {
+    products:[
+      {    
+        name:'banana', 
+        price:'5', 
+        description: 'Fresh bananas from Ecuador'
+      },
+      {    
+        name:'apple', 
+        price:'4', 
+        description: 'Golden apples'
+      },
+      {    
+        name:'pear', 
+        price:'8', 
+        description: 'Sweet pears!'
+      },
+    ],
+  }
+  render(){
 
-      <Product 
-      name="Pear"
-      price="5$"
-      description="Fresh pears"
-      />
-    </div>
-  );
+    const productComponents = this.state.products.map((product)=>{
+      return (
+        <Product 
+        key={product.name}
+        name={product.name} 
+        price={product.price} 
+        description={product.description}
+        />
+      )
+    });
+
+    return (
+      <div className="App">
+
+        {productComponents}
+
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- src/products/Price.jsx
+++ src/products/Price.jsx
@@ -1,11 +1,40 @@
 import {Component} from "react";
 
 class Price extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            price: `${props.text}$`,
+            exchangeRate: 390,
+        };
+    }
+
+    changeCurrency = ()=>{
+        const { price, exchangeRate } = this.state;
+        let modifiedPrice;
+        if(price.endsWith('$')){
+            modifiedPrice = parseFloat(price)*exchangeRate + '֏';
+        }
+        else {
+            modifiedPrice = parseFloat(price)/exchangeRate + '$';
+        }
+    
+        this.setState({
+            price: modifiedPrice,
+        });
+    };
 
     render(){
-        const {text} = this.props;
+
         return(
-            <span>{text}</span>
+            <span>
+                {this.state.price}
+                <button
+                onClick={this.changeCurrency}
+                >
+                Change the currency
+                </button>
+            </span>
         );
     }
 }
